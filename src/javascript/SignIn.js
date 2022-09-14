5cbde6c229a5cac14c4d0b65921825ee61d0856c
AlexandreCSilva <alecosta.silva12@gmail.com>
2022-09-14 16:07:29 -0300

    basic logo created

--- src/javascript/SignIn.js
+++ src/javascript/SignIn.js
@@ -1,10 +1,50 @@
+import mosca from '../images/mosca.png';
+import styled from 'styled-components';
+import { ContentStyle } from '../stylesheet/models'
+
 function SignIn () {
     return (
         <>
-            teste
+            <Content>
+                <Logo>
+                    <img src={mosca} />
+                    <h1>Mosca</h1>
+                    <h1>Atacadista</h1>
+                </Logo>
+
+                <form>
+                    
+                </form>
+                
+            </Content>
         </>
     );
 };
 
-
 export { SignIn };
+
+const Content = styled(ContentStyle)`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Logo = styled.div`
+    position: relative;
+    left: 45px;
+
+    img {
+        position: absolute;
+        height: 100px;
+        transform: rotate(45deg);
+        top: -40px;
+        left: -90px;
+    }
+    
+    h1 {
+        color: white;
+        font-size: 40px;
+    }
+`;
---
+++ src/stylesheet/models.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const ContentStyle = styled.div`
+    background: linear-gradient(to top, #AEA972, #6A8E7F );
+`;
+
+const LogoStyle = styled.div`
+    
+`;
+
+export { ContentStyle }

5657b6acfc648e0d14ec8e64251b3c3ecf01227a
AlexandreCSilva <alecosta.silva12@gmail.com>
2022-09-18 16:06:37 -0300

    basic layout created

--- src/javascript/Cart.js
+++ src/javascript/Cart.js
@@ -1,15 +1,20 @@
 import mosca from '../images/mosca.png';
 import styled from 'styled-components';
-import { ContentStyle, LogoStyle, MenuStyle, SelectionStyle, SellingLongStyle} from '../stylesheet/models';
+import { ContentStyle, LogoStyle, MenuStyle, SelectionStyle} from '../stylesheet/models';
 import { useNavigate, Link } from "react-router-dom";
+import { useEffect } from 'react';
 
 function Cart (){
     const navigate = useNavigate();
-    let cartProducts = [{_id: 'teste', url: ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSi58uIlj8FdzAtwUxZmvuSsZZ37efS_H0naQ&usqp=CAU'], promotion: 10, value: 22.5, name: 'camisa'}];
+    let cartProducts = [{_id: 'teste', url: ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSi58uIlj8FdzAtwUxZmvuSsZZ37efS_H0naQ&usqp=CAU'], promotion: 10, value: 22.5, name: 'camisa'}, {_id: 'teste', url: ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSi58uIlj8FdzAtwUxZmvuSsZZ37efS_H0naQ&usqp=CAU'], promotion: 0, value: 22.5, name: 'camisa'}];
 
     const selectCategory = (event) => {
         const category = event.target.value;
-        navigate('/products/?category='+category)
+        navigate('/products/?category='+category);
+    }
+
+    const deleteProduct = (productId) => {
+        console.log(productId);
     }
 
     return (
@@ -38,10 +43,6 @@ function Cart (){
                         </select>
                     </Selection>
                     
-                    <Link to='/cart'>
-                        <ion-icon name="cart"></ion-icon>
-                    </Link>
-
                     <Link to='/user'>
                         <ion-icon name="person"></ion-icon>
                     </Link>
@@ -55,17 +56,20 @@ function Cart (){
                                     <Link to={'/product/'+product._id} >
                                         <div>
                                             <img src={product.url[0]}/>
+                                            {product.promotion !== 0 ? (<div className="promotion">{product.promotion+'%'}</div>) : ''}
                                         </div>  
                                         <div>
                                             <h2>{product.name}</h2>
-                                            <h3>{product.value}</h3>
+                                            <h3>{product.promotion !== 0 ? product.value - ((product.value * product.promotion)/ 100) : product.value}</h3>
                                         </div>
                                         <div>
                                             <span>
-
+                                                <Link to={'/confirm/'+product._id} >
+                                                    <ion-icon name="checkmark-outline"></ion-icon>
+                                                </Link>
                                             </span>
-                                            <span>
-                                                
+                                            <span onClick={(e) => {e.preventDefault(); console.log(product._id)}}>
+                                                    <ion-icon name="close-outline"></ion-icon>
                                             </span>
                                         </div>
                                     </Link>
@@ -106,6 +110,114 @@ const Menu = styled(MenuStyle)``;
 
 const Selection = styled(SelectionStyle)``;
 
-const SelectionLong = styled(SellingLongStyle)`
+const SelectionLong = styled.div`
+    margin-top: 190px;
+    width: 100%;
 
+    
+    & div {
+        background-color: white;
+        width: 90%;
+        height: 200px;
+        border-radius: 15px;
+        margin-left: 5%;
+        margin-bottom: 40px;
+    }
+    
+    a {
+        text-decoration: none;
+        color: black;
+
+        div {
+            float: left;
+            width: 33.3%;
+            margin: 0;
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            overflow: hidden;
+            border-radius: 15px 0 0 15px;
+
+            img {
+                border-radius: 15px 0 0 15px;
+                height: 100%;
+                width: 100%;
+            }
+
+            .promotion {
+                display: flex;
+                position: absolute;
+                bottom: -16px;
+                right: -14px;
+                font-family: 'Lobster';
+                overflow: hidden;
+                padding: 12px;
+                font-size: 48px;
+                color: white;
+                background-color: #d45c3e;
+                border-radius: 50%;
+                text-shadow: 2px 2px 4px black;
+                transform: rotate(-15deg);
+                height: 60px;
+                width: 90px;
+            }
+
+            h2 {
+                margin-left: 20px;
+                margin-top: 10px;
+                font-size: 34px;
+            }
+
+            h3 {
+                margin-bottom: 12px;
+                margin-left: 35.3%;
+                font-size: 24px;
+                text-align: end;
+                color: #d45c3e;
+            }
+        }
+    }
+
+    span {
+        color: white;
+
+        a {
+            color: white;
+            stroke-width: 32px;
+        }
+    }
+    
+    a :nth-child(3) {
+        width: 10%;
+        display: flex;
+        flex-direction: column;
+        border-radius: 0 15px 15px 0;
+
+        & :nth-child(1) {
+            height: 50%;
+            width: 100%;
+            display: flex; 
+            align-items: center;
+            background-color: #6A8E7F;
+            border-radius: 0 15px 0 0;
+        }
+        
+        &  :nth-child(2) {
+            height: 50%;
+            width: 100%;
+            display: flex; 
+            align-items: center;
+            background-color: #d45c3e;
+            border-radius: 0 0 15px 0;
+
+            ion-icon {
+                background-color: #d45c3e;
+            }
+        }
+    }
+
+    a :nth-child(2) {
+        width: 56.6%;
+    }
 `;
